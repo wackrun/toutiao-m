9411a32b07113b97176886285fbbd955deb5583f
Wack <1361972165@qq.com>
2021-12-29 14:19:28 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入Vant组件库
+import Vant from 'vant'
+// 导入Vant全局样式
+import 'vant/lib/index.css'
 
 // 加载全局样式
 import './styles/index.less'
+// 注册使用Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
